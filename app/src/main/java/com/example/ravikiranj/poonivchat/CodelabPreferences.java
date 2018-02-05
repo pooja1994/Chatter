58cf1a1367e39c51a0af635cd2a4aeb646cb66b3
Ravi Kiran J <25439332+ravikiran1995@users.noreply.github.com>
2018-02-05 11:02:27 +0530

    Revert "Initial commit"
    This reverts commit a90f7adf6c789bc82e4247af0c37d0e9a1c73236.

--- app/src/main/java/com/example/ravikiranj/poonivchat/CodelabPreferences.java
+++
@@ -1,9 +0,0 @@
-package com.example.ravikiranj.poonivchat;
-
-
-public class CodelabPreferences {
-
-    public static final String INSTANCE_ID_TOKEN_RETRIEVED = "iid_token_retrieved";
-    public static final String FRIENDLY_MSG_LENGTH = "friendly_msg_length";
-
-}
